1ca84223655c0bef259d90831d02308c895b7f0c
Jon-Martin Heiberg <heijon17@student.westerdals.no>
2019-11-19 22:21:42 +0100

    added booking

--- src/app/room-details/room-details.page.ts
+++ src/app/room-details/room-details.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import IRoom from '../models/IRoom';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
+import { Platform, ModalController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { DatepickerPage } from '../modals/datepicker/datepicker.page';
 
 
 export interface IRoomId extends IRoom { id: string; }
@@ -21,7 +22,8 @@ export class RoomDetailsPage implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private platform: Platform,
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private modalcontroller: ModalController
   ) {
     this.route.queryParams.subscribe(_ => {
       if (this.router.getCurrentNavigation().extras.state) {
@@ -36,8 +38,30 @@ export class RoomDetailsPage implements OnInit {
 
 
 
-  async bookRoom() {
+  async showDateModal() {
+    const modal = await this.modalcontroller.create({
+      component: DatepickerPage,
+      componentProps: {
+        fromDate: this.roomData.fromDate,
+        toDate: this.roomData.toDate,
+        newRoom: false
+      }
+    });
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+    if (data.status === 'ok') {
+      // handle dates
+      const dates = {};
+      this.bookRoom(dates);
+    }
+  }
 
+  async bookRoom(dates: any) {
+    // TODO: implement support for dates booked.
+    const roomRef = this.firestore.collection<IRoom>('rooms').doc(this.roomData.id);
+    return roomRef.update({
+      available: false
+    });
   }
 
 
